c15d4a8c6abd6e912d94419e9e3a5d0fe3de9dda
Yogesh Bhagat <yogeshbhagat42code@gmail.com>
2026-06-01 16:03:48 +0530

    add support for library content

--- core/src/main/java/org/openedx/core/domain/model/Block.kt
+++ core/src/main/java/org/openedx/core/domain/model/Block.kt
@@ -132,6 +132,7 @@ data class Block(
     val isWordCloudBlock get() = type == BlockType.WORD_CLOUD
     val isLTIConsumerBlock get() = type == BlockType.LTI_CONSUMER
     val isSurveyBlock get() = type == BlockType.SURVEY
+    val isLibraryContentBlock get() = type == BlockType.LIBRARY_CONTENT
 }
 
 @Parcelize
